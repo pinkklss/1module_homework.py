20a19b26c1d289ef0b21bfc261ef97c9f7c90668
pinkklss <shaker_04@mail.ru>
2024-05-28 20:18:02 +0700

    Add files via upload

--- homework4.py
+++ homework4.py
@@ -1,7 +1,16 @@
-my_string=(input())
+my_string = (input())
 print(len(my_string))
 print(my_string.upper())
 print(my_string.lower())
-print (my_string .replace(' ',''))
+print(my_string.replace(' ', ''))
 print(my_string[0])
 print(my_string[-1])
+
+immutamle_var = 1, 1.5, 'number'
+print(immutamle_var)
+immutamle_var[0]=200 #Кортеж не поддерживает изменение в его элементах
+
+mutable_list = ([2, 1.5, 'number'], 0)
+print(mutable_list)
+mutable_list[0][0] = 3
+print(mutable_list)

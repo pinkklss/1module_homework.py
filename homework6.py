c411a568a62aa93177d36317c4a994f29dcd65f6
pinkklss <shaker_04@mail.ru>
2024-05-29 21:55:56 +0700

    homework6.py

--- homework6.py
+++ homework6.py
@@ -1,10 +1,10 @@
-my_dict={'Evgeniy':2004, 'Konstantin':2001, 'Vladislav':2002 }
+my_dict = {'Evgeniy': 2004, 'Konstantin': 2001, 'Vladislav': 2003}
 print(my_dict)
-print(my_dict ['Evgeniy'])
-my_dict['Matvey']= 2002
+print(my_dict['Evgeniy'])
+my_dict['Matvey'] = 2002
 print(my_dict['Matvey'])
 my_dict.update({'Danila': 2002,
-                'Egor':2001})
-del my_dict['Vladislav']
-print(my_dict.get('Vladislav'))
+                'Egor': 2001})
+a = my_dict.pop('Vladislav')
+print(a)
 print(my_dict)
